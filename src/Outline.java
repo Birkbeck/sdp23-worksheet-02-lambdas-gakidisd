a34f5eb4c7d78e1e76bc2758562b489d85a93f54
dimgak <gakidisd@gmail.com>
2023-02-05 15:51:16 +0000

    Built my own array of sample words

--- src/Outline.java
+++ src/Outline.java
@@ -1,9 +1,25 @@
 import java.util.Arrays;
 
 public class Outline {
-  public static void main(String... args) { // varargs alternative to String[]
-    Integer[] intArray = {1,7,3,4,8,2};
-    System.out.println(Arrays.asList(intArray));
-    // Arrays.sort(intArray,.......)
-  } 
+
+  public static String[] getWords() {
+
+    return new String[]{
+            "Fundamentals of Computing",
+            "Python",
+            "Java",
+            "Operation Systems",
+            "Information Systems",
+            "Cloud Computing",
+            "Machine Learning",
+            "Software Design and Programming",
+            "Dissertation Project"
+    };
+
+  }
+
+
+  public static void main(String[] args) {
+
+  }
 }
